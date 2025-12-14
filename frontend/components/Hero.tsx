eab6b6f271893b6d58ca77abfb21232b2dc0f391
Nathan <nathan.sujatno@gmail.com>
2025-12-13 21:03:22 -0600

    changed link to github repo on home page

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -128,7 +128,7 @@ export default function Hero() {
 								</button>
 							</Link>
 
-							<Link href="/dashboard">
+							<Link href="https://github.com/SimplifyJobs/Summer2026-Internships" target="_blank">
 								<button className="flex items-center justify-center gap-2 bg-slate-800 hover:bg-slate-700 text-slate-200 border border-slate-700 px-8 py-3.5 rounded-xl font-semibold transition-all cursor-pointer">
 									<Search size={20} />
 									Browse Jobs
